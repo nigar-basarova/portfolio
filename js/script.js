80342e814e533fd85cbcbae8955a6ee7c7b743a6
nigar-basarova <nigarbasarova@gmail.com>
2024-02-06 22:24:16 +0400

    done

--- js/script.js
+++ js/script.js
@@ -7,4 +7,18 @@ const swiper = new Swiper(".mySwiper", {
     delay: 2500,
     disableOnInteraction: false,
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
 });
